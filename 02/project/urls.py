dd80a991ddab236f580adbae1768c6763b982364
irohandev <rohansingh.rs608@gmail.com>
2025-05-26 13:18:46 +0530

    Comments added

--- 02/project/urls.py
+++ 02/project/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import path
 from app_1 import views  # Importing views from the app named 'app_1'
 
+# from app_1.views import function_1  - we can do this also by importing the specific view function 'function_1' from 'app_1.views'
+
 urlpatterns = [
     # This URL pattern is for the Django admin panel. It connects the '/admin/' route to the default Django admin interface.
     path('admin/', admin.site.urls),
