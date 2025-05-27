6e5997f187c8e64b5898c69b70eba67aa7d79cdf
irohandev <rohansingh.rs608@gmail.com>
2025-05-27 17:21:45 +0530

    More about URL Patterns added

--- 03-URL Patterns/project/urls.py
+++ 03-URL Patterns/project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_1.urls')),  # Include URLs from app_1
-    path('', include('app_2.urls')),  # Include URLs from app_2
+    path('app_2/', include('app_2.urls')),  # Include URLs from app_2 i.e localhost:3000/app_2/
 ]
