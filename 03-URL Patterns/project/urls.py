7d29e7967584bb2fc0cdfd374669e0723fb34233
irohandev <rohansingh.rs608@gmail.com>
2025-05-27 17:04:15 +0530

    Added app 1 , app 2 urls

--- 03-URL Patterns/project/urls.py
+++ 03-URL Patterns/project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app_1.urls')),  # Include URLs from app_1
+    path('', include('app_2.urls')),  # Include URLs from app_2
 ]
