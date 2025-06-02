25ba2d949a20d640ab639f9a66b16fcff084226b
irohandev <rohansingh.rs608@gmail.com>
2025-06-02 18:39:40 +0530

    Added more comments

--- 14-Django Form API/02-Rendering form feilds manually/student/urls.py
+++ 14-Django Form API/02-Rendering form feilds manually/student/urls.py
@@ -1,7 +1,16 @@
+# Importing the `path` function to define URL patterns
 from django.urls import path
+
+# Importing the view functions `registration` and `address` from the `student.views` module
 from student.views import registration, address
 
+# List of URL patterns for routing
 urlpatterns = [
+    # When the user visits the URL ending in 'register/', call the `registration` view
+    # This is typically used for handling student registration logic
     path('register/', registration, name='registration'),
+
+    # When the user visits the URL ending in 'address/', call the `address` view
+    # This view might be used to show or submit a student's address
     path('address/', address, name='address'),
 ]
